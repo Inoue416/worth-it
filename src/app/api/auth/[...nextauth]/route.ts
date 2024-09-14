b1966c6d99b267902643577f82632e644494590c
i-yuya <yuyatuta@gmail.com>
2024-09-15 01:56:14 +0900

    optionsを移動 [buildエラー解消]

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,5 @@
-import NextAuth, { type AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-// import TwitterProvider from "next-auth/providers/twitter"
-
-export const authOptions: AuthOptions = {
-	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-		}),
-		// TwitterProvider({
-		//   clientId: process.env.TWITTER_CLIENT_ID!,
-		//   clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-		// }),
-	],
-	secret: process.env.NEXTAUTH_SECRET,
-	session: {
-		strategy: "jwt",
-		maxAge: 30 * 24 * 60 * 60, // 30 days
-	},
-	callbacks: {
-		async signIn({ user, account, profile, email, credentials }) {
-			return true;
-		},
-		async session({ session, token }) {
-			// Send properties to the client, like an access_token and user id from a provider.
-			session.user = { ...token };
-			return session;
-		},
-		async jwt({ token, trigger, session }) {
-			if (trigger === "update") token.user = session.user;
-			return token;
-		},
-	},
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/authOptions";
 
 const handler = NextAuth(authOptions);
 
