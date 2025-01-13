69a38f9f8b2a4bbb914773b7fc781a16399098b5
i-yuya <yuyatuta@gmail.com>
2025-01-13 17:05:11 +0900

    buildエラー解消

---
+++ src/app/managed-posts/edit/edit-page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Loading from "@/components/loading/Loading";
+import PostForm from "@/components/post-form/PostForm";
+import { useMyPostGetById } from "@/hooks/useMyPostGetById";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
+const EditPage = () => {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const postId = searchParams.get("postId");
+	if (!postId || postId === "") {
+		toast.error("不正なアクセスです");
+		router.push("/");
+	}
+
+	const { post, isLoading, fetchMyPost } = useMyPostGetById();
+
+	useEffect(() => {
+		fetchMyPost(Number(postId));
+	}, [fetchMyPost, postId]);
+
+	if (isLoading || !post) {
+		return <Loading />;
+	}
+	return <PostForm title="投稿の編集" defaultPost={post} />;
+};
+
+export default EditPage;
